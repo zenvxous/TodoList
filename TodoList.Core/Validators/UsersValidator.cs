89cf0d8a5570d1f1d88d83f02fba80b413c6504a
zenvxous <zenvxous@gmail.com>
2024-11-13 10:48:52 +0300

    updated UsersValidator.cs

--- TodoList.Core/Validators/UsersValidator.cs
+++ TodoList.Core/Validators/UsersValidator.cs
@@ -5,12 +5,16 @@ namespace TodoList.Core.Validators;
 public static class UsersValidator
 {
     private const int MAX_USERNAME_LENGTH = 50;
+    private const string USERNAME_PATTERN = @"^[a-zA-Z0-9]+$";
     private const string EMAIL_PATTERN = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
     
     public static string Validate(string username, string email)
     {
         if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(email))
             return "Username and/or email address are required!";
+        
+        if(!IsUsernameValid(username))
+            return "Invalid username!";
         
         if (username.Length > MAX_USERNAME_LENGTH)
             return "Username is too long!";
@@ -20,7 +24,13 @@ public static class UsersValidator
         
         return string.Empty;
     }
-    
+
+    private static bool IsUsernameValid(string username)
+    {
+        return Regex.IsMatch(username, USERNAME_PATTERN);
+    }
+
+
     private static bool IsEmailValid(string email)
     {
         return Regex.IsMatch(email, EMAIL_PATTERN);
